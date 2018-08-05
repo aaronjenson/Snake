2042a2390b18ef670d57d3e3d6f03e8756d986b8
Aaron Jenson <aaronajenson@gmail.com>
2018-08-04 23:06:46 -0700

    Added basic scoring

--- game.js
+++ game.js
@@ -1,16 +1,20 @@
 class Game {
-  constructor(canvas, width, height, cellSize, fps) {
+  constructor(canvas, width, height, cellSize, fps, callback) {
     this.canvas = canvas instanceof Element ? canvas : document.getElementById(canvas);
     this.width = width;
     this.height = height;
     this.paper = new Raphael(this.canvas, this.width, this.height);
     this.cell = cellSize;
 
+    this.callback = callback;
+
+    this.score = 0;
+
     this.cols = Math.floor(this.width / this.cell);
     this.rows = Math.floor(this.height / this.cell);
     this.fps = fps;
 
-    this.paused = true;
+    this.state = "stopped";
 
     this.snake = new Snake(this.paper, this.cols, this.rows, this.cell);
     this.food = new Food(this.paper, this.cols, this.rows, this.cell);
@@ -23,18 +27,29 @@ class Game {
     this.paper.rect(0, 0, width, height).attr('fill', '#333');
     this.food.show();
     this.snake.show();
-    if(!this.paused) {
-      this.snake.run();
-      if(this.snake.isDead()) {
-        this.paused = true;
-      }
-      if(this.snake.isFed(this.food.loc)) {
-        this.food.rnd(this.snake.tail);
-      }
-    } else {
-      this.paper.text(this.width / 2, this.height / 2, "Press space to toggle gameplay")
-          .attr('font-size', 24).attr('fill', '#0f0');
+    
+    switch(this.state) {
+      case "stopped":
+        this.paper.text(this.width / 2, this.height / 2, "Press space to begin gameplay")
+            .attr('font-size', 24).attr('fill', '#0f0');
+        break;
+      case "paused":
+        this.paper.text(this.width / 2, this.height / 2, "Press space to resume gameplay")
+            .attr('font-size', 24).attr('fill', '#0f0');
+        break;
+      case "running":
+        this.snake.run();
+        if(this.snake.isDead()) {
+          this.state = "stopped";
+        }
+        if(this.snake.isFed(this.food.loc)) {
+          this.food.rnd(this.snake.tail);
+          this.score++;
+        }
+        break;
     }
+
+    this.callback();
   }
 
   setFPS(fps) {
@@ -43,8 +58,29 @@ class Game {
     this.loop = window.setInterval(this.run.bind(this), 1000 / this.fps);
   }
 
+  getScore() {
+    return this.score;
+  }
+
+  isRunning() {
+    return this.state === "running";
+  }
+  isStopped() {
+    return this.state === "stopped";
+  }
+  isPaused() {
+    return this.state === "paused";
+  }
+
   pause() {
-    this.paused = !this.paused;
+    this.state = "paused";
+  }
+  resume() {
+    this.state = "running";
+  }
+  start() {
+    this.state = "running";
+    this.score = 0;
   }
 
   up() {
--- main.js
+++ main.js
@@ -3,14 +3,29 @@ var height = 600;
 var cellSize = 20;
 var fps = 10;
 
+gameCallback = function() {
+  scoreElement.innerHTML = game.getScore();
+  console.log("trying to update score... " + game.getScore());
+}
+
 window.onload = function() {
-  game = new Game('canvas', width, height, cellSize, fps);
+  game = new Game('canvas', width, height, cellSize, fps, gameCallback);
+  
+  scoreElement = document.getElementById('score');
 }
 
 window.onkeydown = function(ev) {
   switch(ev.keyCode) {
     case 32:
-      game.pause();
+      if(game.isStopped()) {
+        game.start();
+      }
+      else if(game.isPaused()) {
+        game.resume();
+      }
+      else if(game.isRunning()) {
+        game.pause();
+      }
       break;
     case 72:
     case 37:
